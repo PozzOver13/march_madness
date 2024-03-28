37eb34d9045441ced2bb2951782610cae097ff15
PozzOver13 <pozz.ste@gmail.com>
2024-03-28 16:18:15 +0100

    update test and create feature script

--- ncaa/ncaa/create_feature.py
+++ ncaa/ncaa/create_feature.py
@@ -1,4 +1,9 @@
+import os
 
+from ncaa.src.loader import Loader
+from ncaa.src.config.general import PATH_DATA_RAW
 
 if __name__ == '__main__':
-    print("Done")
+    loader = Loader()
+    path_file = os.path.join(PATH_DATA_RAW, 'kaggle', 'cbb.csv')
+    df = loader.load_data(path_file)
