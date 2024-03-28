06524b8c8ba88d31d5f15b43aca3bff37159fe4b
PozzOver13 <pozz.ste@gmail.com>
2024-03-28 18:15:20 +0100

    add basic test

--- ncaa/tests/test_loader.py
+++ ncaa/tests/test_loader.py
@@ -5,9 +5,16 @@ from ncaa.src.loader import Loader
 from ncaa.src.config.general import PATH_DATA_RAW
 
 
-def test_load_data():
+def test_load_data_rows():
     loader = Loader()
     path_file = os.path.join(PATH_DATA_RAW, 'kaggle', 'cbb.csv')
     df = loader.load_data(path_file)
 
     assert df.shape[0] == 3523
+
+def test_load_data_columns():
+    loader = Loader()
+    path_file = os.path.join(PATH_DATA_RAW, 'kaggle', 'cbb.csv')
+    df = loader.load_data(path_file)
+
+    assert df.shape[1] == 24
